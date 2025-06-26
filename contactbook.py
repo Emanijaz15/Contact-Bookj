ff8406f0c5628c18d552bc1c8514f20fc97072a2
Emanijaz15 <eijaz7357@gmail.com>
2025-06-26 10:49:04 +0500

    Add files via upload

---
+++ contactbook.py
@@ -0,0 +1,20 @@
+def file(name,phone):
+    with open("contacts.txt","a") as f:
+        f.write(f"{Name},{Phone}\n")
+while True:
+    print("\n1. Add Contact\n2. View Contacts\n3. Exit")
+    user=int(input("Choose: "))
+    if (user==1):
+        Name=input("Name: ")
+        Phone=input("Phone: ")
+        file(Name,Phone)
+    if (user==2):
+        with open("contacts.txt", "r") as f:
+            for line in f:
+                Name, Phone = line.strip().split(",")
+                print(f"Name: {Name}, Phone: {Phone}")
+    elif (user==3):
+        break
+    else:
+        print("Invalid input.")
+        
